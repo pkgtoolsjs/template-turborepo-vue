94d77a6c47a6e098ffbf9cec984f84692b5871a4
hhui64 <907322015@qq.com>
2025-04-08 21:26:05 +0800

    feat: add placeholder for custom ESLint rules in configuration

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -3,6 +3,7 @@ import { defineConfigWithVueTs } from '@vue/eslint-config-typescript'
 import baseVueESLintConfig from 'eslint-config/config-vue'
 
 export default defineConfigWithVueTs(baseVueESLintConfig, {
+  // Add your custom rules here
   files: ['**/*.{ts,mts,tsx,vue}'],
   rules: {}
 })
