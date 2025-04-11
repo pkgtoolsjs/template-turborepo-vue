cd4913b1b644bd47993e995def4d13e492233925
hhui64 <907322015@qq.com>
2025-04-12 02:51:13 +0800

    refactor: update import paths to use scoped packages in ESLint configurations

--- packages/ui/eslint.config.ts
+++ packages/ui/eslint.config.ts
@@ -1,4 +1,4 @@
-import { eslintConfigVue } from 'eslint-config/vue'
+import { eslintConfigVue } from '@template-turborepo-vue/eslint-config/vue'
 
 export default eslintConfigVue(
   // Add your custom rules here:
