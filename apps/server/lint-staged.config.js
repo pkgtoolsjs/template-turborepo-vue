72b5223364b05f147d1cf45281981354db93675d
hhui64 <907322015@qq.com>
2025-04-12 01:48:43 +0800

    refactor: simplify lint-staged and format scripts by removing config references

--- apps/server/lint-staged.config.js
+++ apps/server/lint-staged.config.js
@@ -3,11 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js?(x),json,md}': [
-    'prettier --config ../../prettier.config.js --ignore-path ../../.prettierignore --write'
-  ],
-  '*.ts?(x)': [
-    'eslint',
-    'prettier --parser=typescript --config ../../prettier.config.js --ignore-path ../../.prettierignore --write'
-  ]
+  '*.{js?(x),json,md}': ['prettier --write'],
+  '*.ts?(x)': ['eslint', 'prettier --parser=typescript --write']
 }
--- packages/ui/lint-staged.config.js
+++ packages/ui/lint-staged.config.js
@@ -3,14 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js?(x),json,md}': [
-    'prettier --config ../../prettier.config.js --ignore-path ../../.prettierignore --write'
-  ],
-  '*.{htm?(l),css,scss,sass,less}': [
-    'prettier --config ../../prettier.config.js --ignore-path ../../.prettierignore --write'
-  ],
-  '*.{ts?(x),vue}': [
-    'prettier --config ../../prettier.config.js --ignore-path ../../.prettierignore --write',
-    'eslint'
-  ]
+  '*.{js?(x),json,md}': ['prettier --write'],
+  '*.{htm?(l),css,scss,sass,less}': ['prettier --write'],
+  '*.{ts?(x),vue}': ['prettier --write', 'eslint']
 }
