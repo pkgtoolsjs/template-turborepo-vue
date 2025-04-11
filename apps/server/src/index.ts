a753eaf5801345cdcdcce3687a079ec34275e22f
hhui64 <907322015@qq.com>
2025-04-12 03:17:14 +0800

    feat: add server environment configuration and update start scripts to use env files

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,18 @@
+import Fastify from 'fastify'
+import fastifyMiddie from '@fastify/middie'
+import fastifyCors from '@fastify/cors'
 import { add } from '@template-turborepo-vue/math'
 import helloWorld from './modules/helloWorld'
 
-console.log(helloWorld('typescript!'), add(1, 1))
+const app = Fastify({
+  logger: true
+})
+
+app.register(fastifyMiddie)
+app.register(fastifyCors, {})
+
+app.get('/msg', async (request, reply) => {
+  return { msg: `This is a message from the server! ${helloWorld('typescript!')} ${add(1, 2)}` }
+})
+
+await app.listen({ host: process.env.HOST, port: Number(process.env.PORT) })
