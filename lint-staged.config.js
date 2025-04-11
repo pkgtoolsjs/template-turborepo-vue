89f3be99195b2a3abfbb1eba4ade49231954896a
hhui64 <907322015@qq.com>
2025-04-12 00:50:00 +0800

    feat: remove formatting rules from lint-staged configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,4 @@
  * @filename: lint-staged.config.js
  * @type {import('lint-staged').Configuration}
  */
-export default {
-  '*.{js?(x),ts?(x),json,md,vue,css,scss,sass,less}': 'prettier --write'
-}
+export default {}
