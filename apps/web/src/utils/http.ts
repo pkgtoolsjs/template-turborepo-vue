a79c07ce912e2d07cfe743a0dc71bbe4ad50b7e5
hhui64 <907322015@qq.com>
2025-07-01 04:14:54 +0800

    chore(http): remove unnecessary comments from axios interceptors

--- apps/web/src/utils/http.ts
+++ apps/web/src/utils/http.ts
@@ -6,10 +6,8 @@ const useHttp = () => {
     timeout: 5000
   })
 
-  // 请求拦截器
   axiosInstance.interceptors.request.use(
     (config) => {
-      // 在发送请求之前做些什么，例如添加 token
       const token = localStorage.getItem('token')
       if (token) {
         config.headers['Authorization'] = `Bearer ${token}`
@@ -17,19 +15,15 @@ const useHttp = () => {
       return config
     },
     (error) => {
-      // 对请求错误做些什么
       return Promise.reject(error)
     }
   )
 
-  // 响应拦截器
   axiosInstance.interceptors.response.use(
     (response) => {
-      // 对响应数据做些什么
       return response
     },
     (error) => {
-      // 对响应错误做些什么
       return Promise.reject(error)
     }
   )
